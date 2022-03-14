f732670814f7211abdfa65e3a67e377af6018552
mahdiyarz <m.arbabzi@gmail.com>
2022-03-15 00:55:08 +0330

    Create quiz project

---
+++ lib/ui/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class QuizApp extends StatefulWidget {
+  QuizApp({Key? key}) : super(key: key);
+
+  @override
+  State<QuizApp> createState() => _QuizAppState();
+}
+
+class _QuizAppState extends State<QuizApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
