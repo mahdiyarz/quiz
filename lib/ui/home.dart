2d41a2d95b1683c018a7de37b850fc548c042006
mahdiyarz <m.arbabzi@gmail.com>
2022-03-18 13:59:33 +0330

    Create 3 button for check answers and see next answer

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../model/question.dart';
 
 class QuizApp extends StatefulWidget {
   QuizApp({Key? key}) : super(key: key);
@@ -8,8 +9,94 @@ class QuizApp extends StatefulWidget {
 }
 
 class _QuizAppState extends State<QuizApp> {
+  List questionBank = [
+    Question('This questionText is TRUE and you answer true or false', true),
+    Question('questionText2', false),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Real Citizen"),
+        centerTitle: true,
+        backgroundColor: Colors.blueGrey,
+      ),
+      body: Container(
+        color: Colors.blueGrey.shade400,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Image.asset(
+                "images/flag.png",
+                width: 300,
+                height: 200,
+              ),
+            ),
+            Container(
+              width: 350,
+              height: 200,
+              decoration: BoxDecoration(
+                color: Colors.transparent,
+                border: Border.all(
+                  color: Colors.white,
+                  width: 1,
+                ),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              margin: const EdgeInsets.all(10),
+              padding: const EdgeInsets.all(15),
+              child: Text(
+                questionBank[0].questionText,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 16.9,
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: _checkAnswer,
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(Colors.blueGrey.shade700),
+                  ),
+                  child: const Text(
+                    "True",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: _checkAnswer,
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(Colors.blueGrey.shade700),
+                  ),
+                  child: const Text(
+                    "False",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: _nextQuestion,
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(Colors.blueGrey.shade700),
+                  ),
+                  child: const Icon(Icons.arrow_forward_ios_rounded),
+                ),
+              ],
+            ),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
   }
+
+  _checkAnswer() {}
+
+  void _nextQuestion() {}
 }
